596ba77e767da66b1cf8733685f8a231728431eb
Alois <alois.garnier@hotmail.com>
2023-04-03 09:42:46 +0200

    image test

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 import Catalog from "./Catalog";
+import Login from "./Login";
 
-import "../css/style.css"
-import "../css/sketchy.css"
-import "../css/fontawesome.all.min.css"
+import bgImg from "../img/background-flowers.jpg";
+
+import "../css/style.css";
+import "../css/sketchy.css";
+import "../css/fontawesome.all.min.css";
 
 export default function App() {
 
@@ -20,9 +23,9 @@ export default function App() {
               <button class="btn btn-link my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
             </form>
           </div>
-          <a class="navbar-brand" href="#">Mille Arts</a>
+          <Link class="navbar-brand" to="/catalog">Mille Arts</Link>
           <div class="collapse navbar-collapse navbar-right" id="navbarColor02">
-            <button type="submit" class="btn btn-link"><i class="fa-solid fa-user"></i>&thinsp; Connexion</button>
+            <Link to="/login" class="btn btn-link"><i class="fa-solid fa-user"></i>&thinsp; Connexion</Link>
             <button type="submit" class="btn btn-link"><i class="fa-solid fa-basket-shopping"></i>&thinsp; 0</button>
           </div>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01 #navbarColor02" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,7 +33,13 @@ export default function App() {
           </button>
         </div>
       </nav>
-      <Catalog />
+
+        <Routes>
+          <Route exact path="/" element={<Catalog/>}></Route>
+          <Route exact path="/catalog" element={<Catalog/>}></Route>
+          <Route exact path="/login" element={<Login/>}></Route>
+        </Routes>
+
     </BrowserRouter>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Login() {
+
+    return(
+        <div class="form-group">
+            <label class="col-form-label mt-4">Default input</label>
+            <input type="text" class="form-control" placeholder="Default input"></input>
+        </div>
+                
+    );
+
+}
